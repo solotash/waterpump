e665644c776aa96685d218f465ef802bb45996c3
solomongitau <solomontash@gmail.com>
2023-03-24 11:31:06 +0300

    bug fixed

--- wtrctrl/wtrctrl.ino
+++ wtrctrl/wtrctrl.ino
@@ -14,6 +14,7 @@ const int ledPin =  5; // the LED pin number connected
 char auth[] = BLYNK_AUTH_TOKEN;
 int powerPin;
 int waterPin;
+int counter = 0;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
@@ -21,36 +22,34 @@ char ssid[] = "RIO";
 char pass[] = "research!";
 
 BlynkTimer timer;
-
+// connects to the pump off switch
+BLYNK_WRITE(V0) // Executes when the value of virtual pin 0 changes
+{
+int virtual_pin_value = param.asInt();  
+  if(param.asInt() == 1)
+  {
+    // execute this code if the switch widget is now ON
+    digitalWrite(5,HIGH);  // Set digital pin  HIGH    
+  }
+  else
+  {
+    // execute this code if the switch widget is now OFF
+    digitalWrite(5,LOW);  // Set digital pin LOW        
+  }
+}
+//connects to the pump off switch
 BLYNK_WRITE(V1) // Executes when the value of virtual pin 1 changes
 {
 int virtual_pin_value = param.asInt();  
   if(param.asInt() == 1)
   {
     // execute this code if the switch widget is now ON
-    digitalWrite(5,LOW);  // Set digital pin  HIGH    
+    digitalWrite(4,LOW);  // Set digital pin  HIGH   
   }
   else
   {
     // execute this code if the switch widget is now OFF
-    digitalWrite(5,HIGH);  // Set digital pin LOW        
-  }
-}
-
-BLYNK_WRITE(V3) // Executes when the value of virtual pin 3 changes
-{
-int virtual_pin_value = param.asInt();  
-  if(param.asInt() == 1)
-  {
-    // execute this code if the switch widget is now ON
-    digitalWrite(4,LOW);  // Set digital pin  HIGH
-    
-  }
-  else
-  {
-    // execute this code if the switch widget is now OFF
-    digitalWrite(4,HIGH);  // Set digital pin LOW   
-        
+    digitalWrite(4,HIGH);  // Set digital pin LOW           
   }
 }
 void myTimerEvent()
@@ -64,6 +63,7 @@ void myTimerEvent()
 BLYNK_CONNECTED()
 {
   Blynk.syncVirtual(V1);  // will cause BLYNK_WRITE(V0) to be executed
+  Blynk.syncVirtual(V0);
 }
 
 void setup()
@@ -78,9 +78,10 @@ void setup()
 void loop()
 {
   waterPin = touchRead(13);
+  if (waterPin < 70){
+    counter ++;
+  }
   Blynk.run();
   timer.run();
-  // You can inject your own code or combine it with other sketches.
-  // Check other examples on how to communicate with Blynk. Remember
-  // to avoid delay() function!
+ 
 }
